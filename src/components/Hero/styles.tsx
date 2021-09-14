ae7b8572501adbfe193df046dd58b485932b7a69
nadinejuraschek <nadinejwebdev@gmail.com>
2021-09-14 14:49:14 +0200

    Fix: theme background image

--- src/components/Hero/styles.tsx
+++ src/components/Hero/styles.tsx
@@ -8,12 +8,6 @@ export const ButtonWrapper = styled.div`
 `;
 
 export const Container = styled.div`
-  background: linear-gradient(to right, rgba(0, 0, 0), #00000020),
-    url(${({ theme }) => theme.images.hero});
-  background-color: ${({ theme }) => theme.colors.mainBackgroundColor};
-  background-position: right;
-  background-size: cover;
-
   height: 100vh;
 `;
 
--- src/containers/App/styles.tsx
+++ src/containers/App/styles.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
-import imgBg from '../../images/bakery1.jpeg';
 
 export const Container = styled.div`
-  background: linear-gradient(to right, rgba(0, 0, 0), #00000020), url(${imgBg});
+  background: linear-gradient(to right, rgba(0, 0, 0), #00000020),
+    url(${({ theme }) => theme.images.hero});
   background-color: ${({ theme }) => theme.colors.mainBackgroundColor};
   background-position: right;
   background-size: cover;
